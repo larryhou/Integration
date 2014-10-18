00c35722fe6a072d8872d8cded9f9601740898ce
larryhou <larryhou@foxmail.com>
2014-10-18 23:33:36 +0800

    添加性能测试代码

--- IntegrationTests/IntegrationTests.swift
+++ IntegrationTests/IntegrationTests.swift
@@ -21,21 +21,53 @@ class IntegrationTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
+    func testExample()
+	{
         XCTAssert(true, "Pass")
     }
     
-    func testPerformanceExample()
+    func testPerformanceAdd()
 	{
-        // This is an example of a performance test case.
         self.measureBlock()
 		{
 			for i in 1...1_000_000
 			{
-				println(i)
+				i+i
 			}
         }
     }
+	
+	func testPerformanceSubtract()
+	{
+		self.measureBlock()
+		{
+			for i in 1...1_000_000
+			{
+				i-1
+			}
+		}
+	}
+	
+	func testPerformanceMultiply()
+	{
+		self.measureBlock()
+		{
+			for i in 1...1_000_000
+			{
+				i*i
+			}
+		}
+	}
+	
+	func testPerformanceDivide()
+	{
+		self.measureBlock()
+		{
+			for i in 1...1_000_000
+			{
+				i / 10
+			}
+		}
+	}
     
 }
